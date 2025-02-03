4ef11ae7f2e4c6e3344b5ab8a8b86c8a21d92450
Adwait Aayush <adwaitaayush80@gmail.com>
2025-02-03 23:23:24 +0530

    1

--- Backend/api/main.go
+++ Backend/api/main.go
@@ -1,1 +1,19 @@
 package main
+
+import "net/http"
+
+type App struct {
+	Domain string
+}
+
+var err error
+
+func main() {
+	var app App
+	app.Domain = "example.com"
+
+	err := http.ListenAndServe(":6969",app.router())
+	if err!=nil{
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module resultcalc
+
+go 1.23.4
