7e70ad09f2899547152b401787daecfe7ef7a190
Adwait Aayush <adwaitaayush80@gmail.com>
2025-02-04 20:17:08 +0530

    table created

--- Backend/api/table.sql
+++ Backend/api/table.sql
@@ -1,9 +1,6 @@
-CREATE TABLE User(
-    User_id   SERIAL INT PRIMARY KEY ,
+CREATE TABLE public.User(
+    User_id SERIAL PRIMARY KEY,
     Username VARCHAR(100) NOT NULL,
     Userpass VARCHAR(100) NOT NULL,
     Semester INT NOT NULL
 );
-
-INSERT INTO User (Username, Userpass, Semester)
-VALUES ('user1', 'pass1', 1);
