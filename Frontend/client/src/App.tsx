c983e1a7bc03c02fc966a831a635ee57f27332ec
antiquark007 <141497727+antiquark007@users.noreply.github.com>
2025-02-05 23:46:03 +0530

    updted

--- Frontend/client/src/App.tsx
+++ Frontend/client/src/App.tsx
@@ -1,23 +1,10 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
-import Body from './components/Body';
-import CGPA from './pages/CGPA';
-import SGPA from './pages/SGPA';
+import Router from "./routes/Router";
 
 const App: React.FC = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/body" element={<Body />} />
-        <Route path="/cgpa" element={<CGPA />} />
-        <Route path="/sgpa" element={<SGPA />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Router />
     </div>
   );
 };
